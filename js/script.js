b672bf3c880e060a92de11f29bc715b03f28ee52
laim0302 <laim0302@mail.ru>
2021-02-16 17:52:38 +0300

    Validation added

--- js/script.js
+++ js/script.js
@@ -1,57 +1,29 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-	//---------------------------------------
-	// БУРГЕР-МЕНЮ
-	// document.querySelector('.burger-icon').addEventListener('click', function () {
-	// 	document.querySelector('.nav-mobil').classList.toggle('nav-mobil_ative');
-	// 	document.querySelector('.burger-icon_open').classList.toggle('burger-icon_close');
-	// });
-	//---------------------------------------
 
+	// Маскирование полей ввода
+	var selector = document.querySelector("input[type='tel']");
+	var im = new Inputmask("+7 (999) 999-99-99");
+	im.mask(selector);
 
-
-	//------------------------------------------------------
-	// МОДАЛЬНОЕ ОКНО "ЗАКАЗАТЬ ЗВОНОК"
-
-	var btnCallRequest = document.querySelectorAll('.btn__call-request'),  // Кнопка вызова окна
-		modalContainer = document.querySelector('.modal-container'),  // Подложка и само окно
-		modalWindow = document.querySelector('.modal-window'),  // Само окно
-		btnClose = document.querySelector('.modal-window__btn-close'),  // Кнопка закрытия окна без отправки данных
-		btnSend = document.querySelector('.form__btn'),  // Кнопка отправки данных формы
-		messageSent = document.querySelector('.message-sent'),  // Сообщение об отправке данных
-		btnMessageSent = document.querySelector('.message-sent__btn'),  // Кнопка ОК
-		formName = document.getElementById('form__name'), // Поле ввода "Имя"
-		formPhone = document.getElementById('form__phone');  // Поле ввода "Телефон"
-
-	// Вызов окна
-	btnCallRequest.forEach(function (button) {
-		button.addEventListener('click', function (event) {
-			// event.preventDefault();
-			// console.log(this.innerHTML);
-			modalContainer.classList.toggle('modal-container_active');
-		})
+	// Валидация полей ввода
+	new JustValidate('.callback__form', {
+		rules: {
+			name: {
+				required: true,
+				minLength: 2
+			},
+			phone: {
+				required: true,
+				function: (name, value) => {
+					const phoneNumber = selector.inputmask.unmaskedvalue();
+					return Number(phoneNumber) && phoneNumber.length === 10
+				}
+			},
+		},
+		messages: {
+			name: 'Заполните поле',
+			phone: 'Заполните поле'
+		},
 	});
-
-	// Закрытие окна без отправки данных
-	btnClose.addEventListener('click', function () {
-		modalContainer.classList.toggle('modal-container_active');
-	});
-
-	// Отправка данных "Заказать звонок"
-	btnSend.addEventListener('click', function (event) {
-		if (formName.value.length !== 0 && formPhone.value.length !== 0) {
-			modalWindow.classList.toggle('modal-window_close');
-			messageSent.classList.toggle('message-sent_active'); //  Открытие окна "Сообщение отправлено"
-			// console.log(formName.value.length);
-			// console.log(formPhone.value.length);
-		};
-	});
-
-	// Закрытие модального окна "Заказать звонок" после отправки данных
-	btnMessageSent.addEventListener('click', function () {
-		modalWindow.classList.toggle('modal-window_close');
-		messageSent.classList.toggle('message-sent_active');
-		modalContainer.classList.toggle('modal-container_active');
-	});
-	//------------------------------------------------------
 });
